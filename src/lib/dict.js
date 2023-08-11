ff82bb1465d330ba57ad61fcd6c8ed60aa27f1a1
Hakan Alpay <hak7alp@gmail.com>
2023-08-10 18:02:35 -0700

    trying vercel again

--- src/lib/dict.js
+++ src/lib/dict.js
@@ -1,22 +1,22 @@
-import chineseTokenizer from 'chinese-tokenizer';
-// import cedict from './cedict_ts.u8';
-// //  console.log(process.cwd());
-export const tokenize = chineseTokenizer.loadFile('./src/lib/cedict_ts.u8');
+// import chineseTokenizer from 'chinese-tokenizer';
+// // import cedict from './cedict_ts.u8';
+// // //  console.log(process.cwd());
+// export const tokenize = chineseTokenizer.loadFile('./src/lib/cedict_ts.u8');
 
 
-// import fs from 'fs';
-// import path from 'path';
+// // import fs from 'fs';
+// // import path from 'path';
 
-// // Get the URL of the current module
-// const url = new URL(import.meta.url);
+// // // Get the URL of the current module
+// // const url = new URL(import.meta.url);
 
-// // Convert the URL to a file path
-// const dir = path.dirname(url.pathname);
-// // Define the path to your .u8 file in the same directory as this script
-// const filePath = path.join(dir, '..', 'cedict_ts.u8');
+// // // Convert the URL to a file path
+// // const dir = path.dirname(url.pathname);
+// // // Define the path to your .u8 file in the same directory as this script
+// // const filePath = path.join(dir, '..', 'cedict_ts.u8');
 
-// // Read the file as a Buffer
-// const fileBuffer = fs.readFileSync(filePath);
+// // // Read the file as a Buffer
+// // const fileBuffer = fs.readFileSync(filePath);
 
-// // Assuming that chineseTokenizer.loadFile accepts a Buffer or a string
-// export const tokenize = chineseTokenizer.loadFile(fileBuffer);
+// // // Assuming that chineseTokenizer.loadFile accepts a Buffer or a string
+// // export const tokenize = chineseTokenizer.loadFile(fileBuffer);
--- src/routes/tokenize/+server.js
+++ src/routes/tokenize/+server.js
@@ -2,7 +2,8 @@
 //  console.log(process.cwd());
 // const tokenize = chineseTokenizer.loadFile('./src/lib/cedict_ts.u8');
 
-import { tokenize } from '$lib/dict.js';
+// import { tokenize } from '$lib/dict.js';
+import tokenize  from 'chinese-tokenizer-module';
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request, cookies }) {
